2988ab5e04875a6062e58f29773d720a0ab731e1
Christine He <102003703+beycaCC@users.noreply.github.com>
2022-05-16 15:27:22 -0400

    Add files via upload

--- Vocabulary/vocabulary.py
+++ Vocabulary/vocabulary.py
@@ -23,10 +23,11 @@ meaning_list.append("降低(地位, 职位, 威望或尊严) -- to lower in rank
 meaning_list.append("使尴尬, 使羞愧 -- to destroy the self-possession or self-confidence of, disconcert embarrass")
 meaning_list.append("减轻(程度或强度); 减少(数量), 降低(价值) -- to reduce in degree or intensity; to reduce in amount or value")
 meaning_list.append("缩写, 缩短 -- to make brifer")
-meaning_list.append("")
-meaning_list.append("")
-meaning_list.append("")
-meaning_list.append("")
+meaning_list.append("正式放弃(权利, 责任), 不干了 -- to renounce a throne, to relinquish (power or responsibility) formally")
+meaning_list.append("异常的, 非常规的 -- deviating from the usual or natural type")
+meaning_list.append("鼓励 -- to actively encourage (as an activity or plan)")
+meaning_list.append("中止, 搁置 -- temporary inactivity")
+meaning_list.append("深恶痛绝, 极度厌恶 -- to regard with extreme repugnance")
 
 # use a loop to print each meaning, then do check of spell and word in word_list
 j = 0
@@ -39,3 +40,10 @@ for i in meaning_list:
     else:
         print("Wrong!")
     j += 1
+
+# TODO:
+# add time limit
+# add if wrong, output the correct answer (repeat later)
+# add to see the answer temporary
+# add a sys of counting the successes, failnesses, and see the answer to build a summary
+# build them as a database
